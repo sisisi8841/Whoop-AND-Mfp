c1f97ae9ab20540388cf21e2cdc4e0cde3bbb31a
sisisi8841 <nevergo0604@gmail.com>
2025-04-29 18:11:46 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+import myfitnesspal
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Сервер працює!"
+def home():
+    return "Whoop + MFP server is running!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+@app.route("/mfp")
+def get_mfp_data():
+    client = myfitnesspal.Client("nevergo0604205", "310700Vlad310700")
+    day = client.get_date()
+    summary = {
+        "calories": day.totals.get("calories", 0),
+        "carbohydrates": day.totals.get("carbohydrates", 0),
+        "fat": day.totals.get("fat", 0),
+        "protein": day.totals.get("protein", 0),
+        "sodium": day.totals.get("sodium", 0),
+        "sugar": day.totals.get("sugar", 0),
+        "water": day.water,
+    }
+    return jsonify(summary)
